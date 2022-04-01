58144ad13edf34ecd93b85afd22bb1ca7a195f37
mnmartinez127 <mnmartinez@up.edu.ph>
2022-04-01 19:16:19 +0800

    fixed movement v2

--- scripts/GameScreen.gd
+++ scripts/GameScreen.gd
@@ -10,73 +10,79 @@ var kanaFreq
 var tutorialMode
 var assistMode
 var voiceRepeatControl
+var timer = 0
 
 onready var basket = get_node("TileMap/Basket")
 onready var pausebutton = get_node("PauseButton")
 onready var leftblock = get_node("TileMap/LeftBlock")
 onready var rightblock = get_node("TileMap/RightBlock")
+onready var timerscreen = get_node("Timer")
 
 var rand = RandomNumberGenerator.new()
 var kanascene = load("res://scenes/Kana.tscn")
 # program loads settings at bootup
 func _ready():
-	load_config()
-	basket.update_lims(kanaLanes)
-	leftblock.position.x = 40 + (80 * basket.leftlim)
-	rightblock.position.x = 200 + (80 * basket.rightlim)
-	print("Game is ready!")
+    load_config()
+    basket.update_lims(kanaLanes)
+    leftblock.position.x = 40 + (80 * basket.leftlim)
+    rightblock.position.x = 200 + (80 * basket.rightlim)
+    print("Game is ready!")
 
 
 func load_config():
-	if config_file.load(CONFIG_PATH) != OK:
-		print("Config file does not exist")
-		init_settings()
-		return
-	else:
-		load_settings()
+    if config_file.load(CONFIG_PATH) != OK:
+        print("Config file does not exist")
+        init_settings()
+        return
+    else:
+        load_settings()
 
 func init_settings():
-	kanaLanes = 3
-	kanaFreq = 50
-	tutorialMode = false
-	assistMode = false
-	voiceRepeatControl = false
-	config_file.set_value("Game","kanaLanes", kanaLanes)
-	config_file.set_value("Game", "kanaFreq", kanaFreq)
-	config_file.set_value("Game", "tutorialMode", tutorialMode)
-	config_file.set_value("Game", "assistMode", assistMode)
-	config_file.set_value("Game", "voiceRepeatControl", voiceRepeatControl)
-	if config_file.save(CONFIG_PATH) != OK:
-		print("Error: Unable to save configuration file.")
+    kanaLanes = 3
+    kanaFreq = 50
+    tutorialMode = false
+    assistMode = false
+    voiceRepeatControl = false
+    config_file.set_value("Game","kanaLanes", kanaLanes)
+    config_file.set_value("Game", "kanaFreq", kanaFreq)
+    config_file.set_value("Game", "tutorialMode", tutorialMode)
+    config_file.set_value("Game", "assistMode", assistMode)
+    config_file.set_value("Game", "voiceRepeatControl", voiceRepeatControl)
+    if config_file.save(CONFIG_PATH) != OK:
+        print("Error: Unable to save configuration file.")
 
 func load_settings():
-	kanaLanes = config_file.get_value("Game","kanaLanes")
-	kanaFreq = config_file.get_value("Game", "kanaFreq")
-	tutorialMode = config_file.get_value("Game", "tutorialMode")
-	assistMode = config_file.get_value("Game", "assistMode")
-	voiceRepeatControl = config_file.get_value("Game", "voiceRepeatControl")
+    kanaLanes = config_file.get_value("Game","kanaLanes")
+    kanaFreq = config_file.get_value("Game", "kanaFreq")
+    tutorialMode = config_file.get_value("Game", "tutorialMode")
+    assistMode = config_file.get_value("Game", "assistMode")
+    voiceRepeatControl = config_file.get_value("Game", "voiceRepeatControl")
 
 func _unhandled_input(event):
-	if event.is_action_pressed("spawn_key"):
-		var newkana = kanascene.instance()
-		newkana.position.y = 160
-		rand.randomize()
-		var kanacol = rand.randi_range(basket.leftlim ,basket.rightlim)
-		newkana.position.x = 120 + (80 * kanacol)
-		add_child(newkana)
-	if event.is_action_pressed("pause_key"):
-		pausebutton.pausegame()
-	if event.is_action_pressed("quit_key"):
-		if get_tree().change_scene("res://scenes/MainMenuScreen.tscn") != OK:
-			print("Error: Unable to change the scene.")
-	if event.is_action_pressed("left_key"):
-		basket.moveleft()
-	if event.is_action_pressed("right_key"):
-		basket.moveright()
-	get_tree().set_input_as_handled()
+    if event.is_action_pressed("spawn_key"):
+        var newkana = kanascene.instance()
+        newkana.position.y = 160
+        rand.randomize()
+        var kanacol = rand.randi_range(basket.leftlim ,basket.rightlim)
+        newkana.position.x = 120 + (80 * kanacol)
+        add_child(newkana)
+    if event.is_action_pressed("pause_key"):
+        pausebutton.pausegame()
+    if event.is_action_pressed("quit_key"):
+        if get_tree().change_scene("res://scenes/MainMenuScreen.tscn") != OK:
+            print("Error: Unable to change the scene.")
+    if event.is_action_pressed("left_key"):
+        basket.moveleft()
+    if event.is_action_pressed("right_key"):
+        basket.moveright()
+    get_tree().set_input_as_handled()
 
 
 func _on_BackButton_pressed():
-	get_tree().paused = false
-	if get_tree().change_scene("res://scenes/GameOverSreen.tscn") != OK:
-		print("Error: Unable to change the scene.")
+    get_tree().paused = false
+    if get_tree().change_scene("res://scenes/GameOverSreen.tscn") != OK:
+        print("Error: Unable to change the scene.")
+
+func _process(delta):
+    timer += 1
+    timerscreen.text = str((timer-(timer%60))/60)+":"+str(timer%60)
--- scripts/Kanafall.gd
+++ scripts/Kanafall.gd
@@ -7,13 +7,13 @@ var y
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	v = Vector2(0,2)
-	pass # Replace with function body.
+    v = Vector2(0,2)
+    pass # Replace with function body.
 
 
 func _physics_process(_delta):
-	var collide = move_and_collide(v)
-	if collide:
-		print("Collision detected.")
-		print(collide)
-	pass
+    var collide = move_and_collide(v)
+    if collide:
+        print("Collision detected.")
+        print(collide)
+    pass
