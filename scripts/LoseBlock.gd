89958296a18ee421a693d003799c2bcc42e690a0
mnmartinez127 <mnmartinez@up.edu.ph>
2022-04-22 23:22:06 +0800

    Updated the kana generator

---
+++ scripts/LoseBlock.gd
@@ -0,0 +1,15 @@
+tool
+extends Area2D
+
+export(String, FILE) var next_scene_path = ""
+
+func _get_configuration_warning() -> String:
+    if next_scene_path == "":
+        return "Error: missing next_scene_path"
+    else:
+        return ""
+
+
+func _on_LoseBlock_body_entered(body):
+    if get_tree().change_scene(next_scene_path) != OK:
+        print("Error: missing next_scene_path")
