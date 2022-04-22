18127695bd4a52d772b68be54f68daf665777fcd
mnmartinez127 <mnmartinez@up.edu.ph>
2022-04-22 23:20:40 +0800

    Updated kana generator

--- scripts/Kanafall.gd
+++ scripts/Kanafall.gd
@@ -4,16 +4,19 @@ extends KinematicBody2D
 var v
 var x
 var y
-
+var kana
+onready var kanalabel = get_node("Kanalabel")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	v = Vector2(0,2)
-	pass # Replace with function body.
-
+    v = Vector2(0,1)
 
 func _physics_process(_delta):
-	var collide = move_and_collide(v)
-	if collide:
-		print("Collision detected.")
-		print(collide)
-	pass
+    var collide = move_and_collide(v)
+    if test_move(transform,v):
+        print("Collision detected.")
+        print(collide)
+    pass
+
+func update_label(kanaval):
+    kanalabel.text = kanaval
+    print(kanaval)
