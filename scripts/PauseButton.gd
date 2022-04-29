361e1fc8fe91f8d1aabc67978716b76e41651654
Densho1 <den4947@gmail.com>
2022-04-29 09:48:32 +0800

    Cleaned of old files and button groups

--- scripts/PauseButton.gd
+++ scripts/PauseButton.gd
@@ -6,14 +6,14 @@ var paused = true
 onready var pausescreen = get_node("PauseScreen")
 onready var backbutton = get_node("BackButton")
 func _ready():
-    pause_mode = Node.PAUSE_MODE_PROCESS
+	pause_mode = Node.PAUSE_MODE_PROCESS
 
 #use this to pause the game
 func _on_PauseButton_pressed():
-    pausegame()
+	pausegame()
 
 func pausegame():
-    paused = !paused
-    get_tree().paused = paused
-    pausescreen.visible = paused
-    backbutton.visible = paused
+	paused = !paused
+	get_tree().paused = paused
+	pausescreen.visible = paused
+	backbutton.visible = paused
