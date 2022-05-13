cd0536000e71c61f71d014c22b89bb68010e46b9
mnmartinez127 <mnmartinez@up.edu.ph>
2022-05-13 09:04:48 +0800

    Fixed kana not being removed on contact with the basket

--- scripts/GameOverSreen.gd
+++ scripts/GameOverSreen.gd
@@ -12,44 +12,44 @@ onready var gameoverlabel = get_node("GameOverLabel")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	loadscore()
-	savescore()
-	displayscore()
+    loadscore()
+    savescore()
+    displayscore()
 
 func loadscore():
-	if score_file.load(SCORE_PATH) != OK:
-		print("Score file does not exist")
-		initscore()
-	rightkana = score_file.get_value("Most Recent Score", "rightkana", rightkana)
-	numkanacaught = score_file.get_value("Most Recent Score", "numkanacaught", numkanacaught)
-	timescore = score_file.get_value("Most Recent Score", "timescore", timescore)
+    if score_file.load(SCORE_PATH) != OK:
+        print("Score file does not exist")
+        initscore()
+    rightkana = score_file.get_value("Most Recent Score", "rightkana", rightkana)
+    numkanacaught = score_file.get_value("Most Recent Score", "numkanacaught", numkanacaught)
+    timescore = score_file.get_value("Most Recent Score", "timescore", timescore)
 
 func savescore():
-	top5scores = score_file.get_value("Scores", "Top5scores")
-	for i in range(5):
-		if top5scores[i][1] >= numkanacaught:
-			if timescore > top5scores[i][0]:
-				if i != 4:
-					continue
-			top5scores.insert(i, [timescore, numkanacaught])
-			break
-		if i == 4:
-			top5scores.insert(5, [timescore, numkanacaught])
-	top5scores = top5scores.slice(1,5)
-	print("scoreboard: "+str(top5scores))
-	score_file.set_value("Scores", "Top5scores", top5scores)
-	if score_file.save(SCORE_PATH) != OK:
-		print("Error: Unable to save configuration file.")
+    top5scores = score_file.get_value("Scores", "Top5scores")
+    for i in range(5):
+        if top5scores[i][1] >= numkanacaught:
+            if timescore > top5scores[i][0]:
+                if i != 4:
+                    continue
+            top5scores.insert(i, [timescore, numkanacaught])
+            break
+        if i == 4:
+            top5scores.insert(5, [timescore, numkanacaught])
+    top5scores = top5scores.slice(1,5)
+    print("scoreboard: "+str(top5scores))
+    score_file.set_value("Scores", "Top5scores", top5scores)
+    if score_file.save(SCORE_PATH) != OK:
+        print("Error: Unable to save configuration file.")
 
 func displayscore():
-	gameoverlabel.text = "Game Over\nKana Caught: "+str(numkanacaught)+"\nTime Elapsed: "+str((int(timescore)-(int(timescore)%60))/60)+":"+str(int(fmod(timescore,60)))
-	
+    gameoverlabel.text = "Game Over\nKana Caught: "+str(numkanacaught)+"\nTime Elapsed: "+str((int(timescore)-(int(timescore)%60))/60)+":"+str(int(fmod(timescore,60)))
+
 func initscore():
-	top5scores = [[5, 0], [10, 1], [20, 2], [30, 3], [40, 4]]
-	score_file.set_value("Scores", "Top5scores", top5scores)
-	if score_file.save(SCORE_PATH) != OK:
-		print("Error: Unable to save configuration file.")
-	
+    top5scores = [[5, 0], [10, 1], [20, 2], [30, 3], [40, 4]]
+    score_file.set_value("Scores", "Top5scores", top5scores)
+    if score_file.save(SCORE_PATH) != OK:
+        print("Error: Unable to save configuration file.")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
--- scripts/Kanafall.gd
+++ scripts/Kanafall.gd
@@ -8,15 +8,15 @@ var kana
 onready var kanalabel = get_node("Kanalabel")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	v = Vector2(0,1)
+    v = Vector2(0,1)
 
 func _physics_process(_delta):
-	var collide = move_and_collide(v)
-	if test_move(transform,v):
-		print("Collision detected.")
-		print(collide)
-	pass
+    var collide = move_and_collide(v)
+    if test_move(transform,v):
+        print("Collision detected.")
+        print(collide)
+    pass
 
 func update_label(kanaval):
-	kanalabel.text = kanaval
-	print(kanaval)
+    kanalabel.text = kanaval
+    print(kanaval)
